c9e152b55c1fb7e7486913730d16db19497a734a
Graham Lipsman <graham@branchlabs.com>
2015-08-19 16:11:06 -0600

    Add default value for sleeps attribute in the task model. Introduce a new boolean 'completed' attribute to track whether or not a task is finished.

--- frontend/app/models/task.js
+++ frontend/app/models/task.js
@@ -3,6 +3,7 @@ import DS from 'ember-data';
 export default DS.Model.extend({
   list: DS.belongsTo('list'),
   content: DS.attr('string'),
-  sleeps: DS.attr('number'),
-  createdAt: DS.attr('date')
+  sleeps: DS.attr('number', { defaultValue: 0 }),
+  createdAt: DS.attr('date'),
+  completed: DS.attr('boolean', { defaultValue: false }),
 });
